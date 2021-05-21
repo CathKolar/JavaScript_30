7c5a5a9922fdab20c32eaa705e82325deb2ba657
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-05-21 11:14:26 +0200

    get elements

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,10 @@
 /* Get our elements */
-
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progressBar = player.querySelector(".progress");
+const toggle = player.querySelector(".progress__filled");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 /* Build our functions */
 
-/* Hook up the event listeners */ 
+/* Hook up the event listeners */
